3968722aed0b553be93de107b9bb67cccde2ea72
Daniel Tchoń <dantch@onet.eu>
2022-06-23 13:18:01 +0200

    Reorganised `test_engine` into `test_calculator`

--- test/test_calculator.py
+++ test/test_calculator.py
@@ -1,11 +1,91 @@
 import pathlib
 import unittest
-
+from pruby.engine import Engine
 from pruby.calculator import PressureCalculator
+from pruby import strategies
 
 
 test_data1_path = str(pathlib.Path(__file__).parent.joinpath('test_data1.txt'))
 test_data2_path = str(pathlib.Path(__file__).parent.joinpath('test_data2.txt'))
+
+subengines = \
+    [
+        'reader',
+        'backfitter',
+        'peakfitter',
+        'corrector',
+        'translator',
+        'drawer'
+    ]
+strategy_types = \
+    [
+        'reading',
+        'peakfitting',
+        'backfitting',
+        'correcting',
+        'translating',
+        'drawing'
+    ]
+strategy_families = \
+    [
+        strategies.ReadingStrategies,
+        strategies.BackfittingStrategies,
+        strategies.PeakfittingStrategies,
+        strategies.CorrectingStrategies,
+        strategies.TranslatingStrategies,
+        strategies.DrawingStrategies
+    ]
+strategy_parents = \
+    [
+        strategies.ReadingStrategy,
+        strategies.BackfittingStrategy,
+        strategies.PeakfittingStrategy,
+        strategies.CorrectingStrategy,
+        strategies.TranslatingStrategy,
+        strategies.DrawingStrategy
+    ]
+
+
+class TestEngine(unittest.TestCase):
+    def test_create_empty(self):
+        self.assertIsNotNone(Engine(calc=PressureCalculator()))
+
+    def test_has_default_strategies(self):
+        e = Engine(calc=PressureCalculator())
+        for subengine in subengines:
+            self.assertTrue(hasattr(e, subengine))
+
+    def test_can_set_null_strategies(self):
+        s = Engine(calc=PressureCalculator())
+        s.set_strategy()
+        for sub in subengines:
+            self.assertIsInstance(getattr(s, sub), strategies.BaseStrategy)
+
+    def test_can_set_real_strategies(self):
+        s1 = Engine(calc=PressureCalculator())
+        s2 = Engine(calc=PressureCalculator())
+        for stype, sfamily in zip(strategy_types, strategy_families):
+            name_of_non_default_strategy = [k for k in sfamily.registry.keys()
+                                            if k is not sfamily.default.name][0]
+            s1.set_strategy(**{stype: name_of_non_default_strategy})
+        for sub in subengines:
+            self.assertNotEqual(type(getattr(s1, sub)), type(getattr(s2, sub)))
+
+    def test_cant_set_unexisting_strategies(self):
+        engine = Engine(calc=PressureCalculator())
+        for sub in subengines:
+            with self.assertRaises(TypeError):
+                engine.set_strategy(**{sub: f'dummy {sub} name'})
+
+    def test_can_assign_real_subengines(self):
+        s1 = Engine(calc=PressureCalculator())
+        s2 = Engine(calc=PressureCalculator())
+        for sub, sfamily in zip(subengines, strategy_families):
+            non_default_strategy = [v for v in sfamily.registry.values()
+                                    if v.name != sfamily.default.name][0]
+            setattr(s1, sub, non_default_strategy)
+        for sub in subengines:
+            self.assertNotEqual(type(getattr(s1, sub)), type(getattr(s2, sub)))
 
 
 # noinspection PyTypeChecker
