06fc5425ba3eed5321ecb008b31e57ca293e3961
Daniel Tchoń <dantch@onet.eu>
2022-06-23 14:36:44 +0200

    `pruby.Spectrum.sigma_type` is not a `SigmaType`-enum-type property

--- pruby/spectrum/spectrum.py
+++ pruby/spectrum/spectrum.py
@@ -1,3 +1,4 @@
+import enum
 import numpy as np
 
 from .curve import Curve
@@ -5,19 +6,31 @@ from ..utility.line_subset import LineSubset
 
 
 class Spectrum:
+    class SigmaType(enum.Enum):
+        equal = 'equal'
+        huber = 'huber'
+
     def __init__(self, x=tuple(), y=tuple(), curve=Curve(),
                  focus=LineSubset(), sigma_type='equal'):
         self.x = np.array(x)
         self.y = np.array(y)
         self.curve = curve
         self.focus = self.domain if focus == LineSubset() else focus
-        self.sigma_type = sigma_type   # 'equal' or 'huber'
+        self.sigma_type = sigma_type
 
     def __len__(self):
         return len(self.x)
 
     def __bool__(self):
         return len(self.x) > 0
+
+    @property
+    def sigma_type(self):
+        return self._sigma_type
+
+    @sigma_type.setter
+    def sigma_type(self, value):
+        self._sigma_type = self.SigmaType(value)
 
     @property
     def f(self):
@@ -29,9 +42,9 @@ class Spectrum:
 
     @property
     def si(self):
-        if self.sigma_type == 'equal':
+        if self.sigma_type == self.SigmaType.equal:
             return np.ones_like(self.x)
-        elif self.sigma_type == 'huber':
+        elif self.sigma_type == self.SigmaType.huber:
             tol = 0.01 * max(abs(self.delta))
             return np.array([tol ** 2 if d < tol else
                              tol * (2 * d - tol) for d in self.delta])
@@ -56,7 +69,7 @@ class Spectrum:
     def within(self, subset):
         in_subset = [True if x in subset else False for x in self.x]
         return Spectrum(self.x[in_subset], self.y[in_subset], curve=self.curve,
-                        focus=subset, sigma_type=self.sigma_type)
+                        focus=subset, sigma_type=self.sigma_type.value)
 
     def focus_on_edge(self, width=1.0):
         sub1 = LineSubset(min(self.x), min(self.x) + width)
